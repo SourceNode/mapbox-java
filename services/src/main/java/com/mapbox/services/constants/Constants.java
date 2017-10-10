a26c01a5894342266d48c22ff571d57398ecbac4
Cameron Mace <cameron@mapbox.com>
2017-10-09 21:17:53 -0700

    resolved buildconfig not being generated

--- services/src/main/java/com/mapbox/services/constants/Constants.java
+++ services/src/main/java/com/mapbox/services/constants/Constants.java
@@ -1,5 +1,7 @@
 package com.mapbox.services.constants;
 
+import com.mapbox.services.BuildConfig;
+
 import java.util.Locale;
 
 /**
@@ -18,15 +20,21 @@ public final class Constants {
    *
    * @since 1.0.0
    */
-  public static final String HEADER_USER_AGENT = "TODO";
-//    String.format(Locale.US, "MapboxJava/%s (%s)", BuildConfig.VERSION, BuildConfig.GIT_REVISION);
+  public static final String HEADER_USER_AGENT
+    = String.format(Locale.US, "MapboxJava/%s (%s)",
+    BuildConfig.VERSION, BuildConfig.GIT_REVISION);
 
   /**
-   * Base URL for all API calls, not hardcoded to enable testing
+   * Base URL for all API calls, not hardcoded to enable testing.
    *
    * @since 1.0.0
    */
   public static final String BASE_API_URL = "https://api.mapbox.com";
 
+  /**
+   * The default user variable used for all the Mapbox user names.
+   *
+   * @since 1.0.0
+   */
   public static final String MAPBOX_USER = "mapbox";
 }
